91d864258c7c4707a00441d9fdd751418e8097e0
Francesco Sacco <francesco_sacco@hotmail.com>
2018-12-31 15:29:54 -0200

    Converts base32 into a library.

--- library/base32.c
+++ library/base32.c
@@ -2,10 +2,9 @@
  * @file bin2base32.c
  * @author Francesco Sacco
  * @date 12 Dec 2018
- * @brief This project converts binary files to Base-32 files.
+ * @brief This project converts binary data to Base-32.
  *
- * This software is a tool to convert binary files to Base-32
- * files.
+ * This library converts binary data to Base-32.
  * @see http://github.com/francescosacco/BinaryTools
  */
 
@@ -13,108 +12,14 @@
   *
   * Version log. 
   *
+  * 2018-12-30 - 0.1.0 - Created library with Base-32 code.
   * 2018-12-12 - 0.0.0 - Initial version.
   *
   **********/
- 
-#include <stdio.h>
+#include "base32.h"
+
 #include <stdint.h>
 #include <string.h>
-
-void bin2base32( uint8_t * binData , uint8_t binDataSize , uint8_t * base32Data ) ;
-
-int main( int argc , char * argv[] )
-{
-    FILE * fileIn  = NULL ;
-    FILE * fileOut = NULL ;
-    fpos_t  fileInSize , i , j ;
-    int chr , ret ;
-    
-    uint8_t bufferBin[ 5 ] ;
-    uint8_t buffer32[ 9 ] ;
-    
-    // Initial messages.
-    printf( "Bin To Base32 - Version 0.0.0\n" ) ;
-    printf( "Francesco Sacco - francesco_sacco@hotmail.com\n" ) ;
-    
-    // Check arguments.
-    if( argc < 3 )
-    {
-        printf( "Usage: bin2base32 <Binary File In> <Base32 File Out>\n" ) ;
-        printf( "\n" ) ;
-        return( 0 ) ;
-    }
-    
-    // Open Binary File.
-    fileIn = fopen( argv[ 1 ] , "rb" ) ;
-    if( fileIn == ( ( FILE * ) NULL ) )
-    {
-        printf( "\tError to open \"%s\"\n" , argv[ 1 ] ) ;
-        return( -1 ) ;
-    }
-
-    // Create Base32 File.
-    fileOut = fopen( argv[ 2 ] , "wb" ) ;
-    if( fileOut == ( ( FILE * ) NULL ) )
-    {
-        fclose( fileIn ) ;
-        printf( "\tError to create \"%s\"\n" , argv[ 2 ] ) ;
-        return( -1 ) ;
-    }
-    
-    // Check binary file size.
-    fileInSize = 0 ;
-    ret = fseek( fileIn , 0 , SEEK_END ) ;
-    ret = ( ret == 0 ) ? fgetpos( fileIn , &fileInSize ) : ( ret ) ;
-    ret = ( ret == 0 ) ? fseek( fileIn , 0 , SEEK_SET ) : ( ret ) ;
-    if( ret != 0 )
-    {
-        ret = ferror( fileIn ) ;
-        printf( "\tError to get binary file size!\n" ) ;
-        printf( "\tferror [ %d ]\n" , ret ) ;
-        fclose( fileIn ) ;
-        fclose( fileOut ) ;
-        return( ret ) ;
-    }
-    printf( "\t\"%s\" - Size = %lu\n" , argv[ 1 ] , ( unsigned long ) fileInSize ) ;
-
-    i = 0 ;
-    while( i < fileInSize )
-    {
-        for( j = 0 ; ( j < 5 ) && ( i < fileInSize ) ; j++, i++ )
-        {
-            chr = getc( fileIn ) ;
-            if( chr == EOF )
-            {
-                ret = ferror( fileIn ) ;
-                printf( "\tError to read binary file at %lu!\n" , ( unsigned long ) i ) ;
-                printf( "\tferror [ %d ]\n" , ret ) ;
-                fclose( fileIn ) ;
-                fclose( fileOut ) ;
-                return( ret ) ;
-            }
-
-            bufferBin[ j ] = ( uint8_t ) chr ;
-        }
-        
-        bin2base32( bufferBin , j , buffer32 ) ;
-        
-        ret = fprintf( fileOut , "%s" , buffer32 ) ;
-        if( ret < 0 )
-        {
-            ret = ferror( fileOut ) ;
-            printf( "\tError to write base32 file!\n" ) ;
-            printf( "\tferror [ %d ]\n" , ret ) ;
-            fclose( fileIn ) ;
-            fclose( fileOut ) ;
-            return( ret ) ;
-        }
-    }
-    
-    printf( "\tDone!\n" ) ;
-    
-    return( 0 ) ;
-}
 
 void bin2base32( uint8_t * binData , uint8_t binDataSize , uint8_t * base32Data )
 {
@@ -122,6 +27,7 @@ void bin2base32( uint8_t * binData , uint8_t binDataSize , uint8_t * base32Data 
                                             'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' ,
                                             'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' ,
                                             'Y' , 'Z' , '2' , '3' , '4' , '5' , '6' , '7' } ;
+
     
     uint8_t binDataCopy[ 5 ] ;
     uint8_t data , binDataCopySize ;
---
+++ library/base32.h
@@ -0,0 +1,26 @@
+/**
+ * @file bin2base32.c
+ * @author Francesco Sacco
+ * @date 12 Dec 2018
+ * @brief This project converts binary data to Base-32.
+ *
+ * This library converts binary data to Base-32.
+ * @see http://github.com/francescosacco/BinaryTools
+ */
+
+ /**********
+  *
+  * Version log. 
+  *
+  * 2018-12-30 - 0.1.0 - Created library with Base-32 code.
+  * 2018-12-12 - 0.0.0 - Initial version.
+  *
+  **********/
+#ifndef _BASE32_H_
+#define _BASE32_H_
+
+#include <stdint.h>
+
+void bin2base32( uint8_t * binData , uint8_t binDataSize , uint8_t * base32Data ) ;
+
+#endif // _BASE32_H_
