1524c6b5471d6132f063a2765560b70a66830238
Francesco Sacco <francesco_sacco@hotmail.com>
2026-04-30 11:36:38 -0300

    Update error returns.

--- apps/binCompare.c
+++ apps/binCompare.c
@@ -13,6 +13,7 @@
   *
   * Version log. 
   *
+  * 2026-04-30 - 1.0.0 - Small changes for new architecture environment.
   * 2019-05-24 - 0.0.3 - Add first byte different.
   * 2019-02-26 - 0.0.2 - Add getFileSize function and difference size.
   * 2018-05-25 - 0.0.1 - Fix data type to print at console.
@@ -22,6 +23,7 @@
  
 #include <stdio.h>
 #include <stdint.h>
+#include <errno.h>
 
 uint32_t getFileSize( FILE * in ) ;
 
@@ -30,16 +32,16 @@ int main( int argc , char * argv[] )
     FILE * file1  ;
     FILE * file2 ;
     uint32_t fileSize1 , fileSize2 , smallestSize , differences , i , addrFirstDif ;
-    int chr1 , chr2 , ret ;
+    int chr1 , chr2 ;
     
     // Initial messages.
-    printf( "Bin Compare - Version 0.0.3\n" ) ;
-    printf( "Francesco Sacco - francesco_sacco@hotmail.com\n" ) ;
-    
+    printf( "bin Compare - Version 1.0.0\n" ) ;
+    printf( "github.com/francescosacco/BinaryTools\n" ) ;    
+
     // Check arguments.
     if( argc < 3 )
     {
-        printf( "Usage: binCompare <Binary File 1> <Binary File 2>\n" ) ;
+        printf( "Usage: %s <Binary File 1> <Binary File 2>\n" , argv[ 0 ] ) ;
         printf( "\n" ) ;
         return( 0 ) ;
     }
@@ -48,17 +50,22 @@ int main( int argc , char * argv[] )
     file1 = fopen( argv[ 1 ] , "rb" ) ;
     if( file1 == ( ( FILE * ) NULL ) )
     {
-        printf( "\tError to open \"%s\"\n" , argv[ 1 ] ) ;
-        return( -1 ) ;
+        printf( "\tError openning %s.\n" , argv[ 1 ] ) ;
+        perror( "\tError" ) ;
+        return errno ;
     }
     
     // Open Binary File 2.
     file2 = fopen( argv[ 2 ] , "rb" ) ;
     if( file2 == ( ( FILE * ) NULL ) )
     {
+        int errno_fopen = errno ;
+
+        printf( "\tError openning %s.\n" , argv[ 2 ] ) ;
+        perror( "\tError" ) ;
+
         fclose( file1 ) ;
-        printf( "\tError to open \"%s\"\n" , argv[ 2 ] ) ;
-        return( -1 ) ;
+        return errno_fopen ;
     }
     
     // Check binary files size.
@@ -89,23 +96,29 @@ int main( int argc , char * argv[] )
         chr1 = getc( file1 ) ;
         if( chr1 == EOF )
         {
-            ret = ferror( file1 ) ;
+            int errno_fopen = errno ;
+
             printf( "\tError to read file \"%s\"at %lu!\n" , argv[ 1 ] , ( unsigned long ) i ) ;
-            printf( "\tferror [ %d ]\n" , ret ) ;
+            perror( "\tError" ) ;
+
             fclose( file1 ) ;
             fclose( file2 ) ;
-            return( ret ) ;
+
+            return( errno_fopen ) ;
         }    
         
         chr2 = getc( file2 ) ;
         if( chr2 == EOF )
         {
-            ret = ferror( file2 ) ;
-            printf( "\tError to read file \"%s\"at %lu!\n" , argv[ 2 ] , ( unsigned long ) i ) ;
-            printf( "\tferror [ %d ]\n" , ret ) ;
+            int errno_fopen = errno ;
+
+            printf( "\tError to read file \"%s\"at %lu!\n" , argv[ 1 ] , ( unsigned long ) i ) ;
+            perror( "\tError" ) ;
+
             fclose( file1 ) ;
             fclose( file2 ) ;
-            return( ret ) ;
+
+            return( errno_fopen ) ;
         }    
 
         if( chr1 != chr2 )
